42732c548246dc56ea2eaeef4f1523df781c2300
sachin1101 <sachinkaulage05@gmail.com>
2025-03-10 20:08:28 +0530

    Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Stok-prediction1/", // ✅ Set correct base path
+  base: "/stock_predication/", // ✅ Set correct base path
   plugins: [react()],
 });
